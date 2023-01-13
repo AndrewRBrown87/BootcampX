9fcb885cde53bb7a7a541cb43fb951e79287b8fe
Andrew Brown <andrew.robert.brown@gmail.com>
2023-01-13 23:12:27 +0000

    Add queries 4

---
+++ 4_queries/6.sql
@@ -0,0 +1,6 @@
+SELECT cohorts.name, avg(assistance_requests.completed_at - assistance_requests.started_at) AS average
+FROM cohorts JOIN students ON cohorts.id = students.cohort_id
+JOIN assistance_requests ON students.id = assistance_requests.student_id
+GROUP BY cohorts.name
+ORDER BY average desc
+LIMIT 1;
