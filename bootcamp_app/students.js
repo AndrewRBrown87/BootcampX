20920f7289c0d82e6fb27a3bdb9452a9b11651a9
Andrew Brown <andrew.robert.brown@gmail.com>
2023-01-19 00:07:08 +0000

    Add SQL Injection Protection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -9,13 +9,15 @@ const pool = new Pool({
 
 const cohort = process.argv[2];
 const numResults = process.argv[3];
-
-pool.query(`
+const values = [`${cohort}%`, numResults];
+const queryString = `
 SELECT students.id, students.name, cohort_id, cohorts.name AS cohort_name
 FROM students JOIN cohorts ON students.cohort_id = cohorts.id
-WHERE cohorts.name LIKE '${cohort}%'
-LIMIT ${numResults};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_name} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,16 +8,18 @@ const pool = new Pool({
 });
 
 const cohort = process.argv[2];
-
-pool.query(`
+const values = [`${cohort}`];
+const queryString = `
 SELECT teachers.name AS teacher, cohorts.name AS cohort, count(assistance_requests.*) AS total_assistances
 FROM teachers JOIN assistance_requests ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = '${cohort}'
+WHERE cohorts.name = $1
 GROUP BY teachers.name, cohorts.name
 ORDER BY teachers.name;
-`)
+`;
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.teacher}`);
